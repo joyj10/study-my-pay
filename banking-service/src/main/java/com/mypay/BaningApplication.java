331ff9c6842c027ccad93fe756aa82e3810562d0
joyj10 <micool1030@gmail.com>
2024-10-20 22:04:56 +0900

    [setting] banking-service 모듈 추가 - 하위 모듈 추가

---
+++ banking-service/src/main/java/com/mypay/BaningApplication.java
@@ -0,0 +1,12 @@
+package com.mypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaningApplication.class, args);
+    }
+
+}
